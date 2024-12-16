b015f7d9975aeba17ccfc1d760c158fd31b234be
34 <34>
2024-12-17 00:14:59 +0300

    login screen fix 2.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart'; // Импортируем пакет для работы с SVG
 import 'package:gazservice/item_list_screen.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -66,33 +67,6 @@ class _AuthScreenState extends State<AuthScreen> {
     }
   }
 
-  Future<void> _register() async {
-    final response = await http.post(
-      Uri.parse('https://gaz-api.webmonkeys.ru'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, String>{
-        'email': _email,
-        'password': _password,
-      }),
-    );
-
-    if (response.statusCode == 201) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => ItemListScreen()),
-      );
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Registration successful!')),
-      );
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Registration failed!')),
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -101,87 +75,98 @@ class _AuthScreenState extends State<AuthScreen> {
         centerTitle: true,
       ),
       body: Container(
-        decoration: BoxDecoration(
-          color: Colors.lightBlue,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Center(
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      filled: true,
-                      fillColor: Colors.black,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: const BorderSide(color: Colors.white, width: 1.0), // Белый контур
-                      ),
-                      contentPadding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your email';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) {
-                      _email = value;
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      filled: true,
-                      fillColor: Colors.black,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: const BorderSide(color: Colors.white , width: 1.0), // Белый контур
-                      ),
-                      contentPadding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
-                    ),
-                    obscureText: true,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your password';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) {
-                      _password = value;
-                    },
-                  ),
-                  const SizedBox(height: 80), // Увеличенный отступ
-                  SizedBox(
-                    width: double.infinity, // Задает ширину кнопки на всю ширину
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          _login();
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.black, // Черный фон
-                        side: const BorderSide(color: Colors.white, width: 1.0), // Белый контур
-                        padding: const EdgeInsets.symmetric(vertical: 15.0),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0), // Круглые углы
-                        ),
-                      ),
-                      child: const Text('Login', style: TextStyle(fontSize: 18)),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                ],
-              ),
+      decoration: BoxDecoration(
+      color: Colors.lightBlue,
+    ),
+    child: Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: Center(
+    child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    // Добавляем SVG логотип
+    SvgPicture.asset(
+    'assets/logo.svg',  // Путь к вашему SVG файлу
+    height: 120, // Установите нужную высоту
+    ),
+    const SizedBox(height: 20), // Отступ между логотипом и формой
+    Form(
+    key: _formKey,
+    child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    TextFormField(
+    decoration: InputDecoration(
+    labelText: 'Email',
+    filled: true,
+    fillColor: Colors.black,
+    border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10.0),
+    borderSide: const BorderSide(color: Colors.white, width: 1.0), // Белый контур
+    ),
+    contentPadding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+    ),
+    validator: (value) {
+    if (value == null || value.isEmpty) {
+    return 'Please enter your email';
+    }
+    return null;
+    },
+    onChanged: (value) {
+    _email = value;
+    },
+    ),
+    const SizedBox(height: 20),
+    TextFormField(
+    decoration: InputDecoration(
+    labelText: 'Password',
+    filled: true,
+    fillColor: Colors.black,
+    border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10.0),
+    borderSide: const BorderSide(color: Colors.white, width: 1.0), // Белый контур
+    ),
+    contentPadding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+    ),
+    obscureText: true,
+    validator: (value) {
+    if (value == null || value.isEmpty) {
+    return 'Please enter your password';
+    }
+    return null;
+    },
+    onChanged: (value) {
+    _password = value;
+    },
+    ),
+      const SizedBox(height: 80), // Увеличенный отступ
+      SizedBox(
+        width: double.infinity, // Задает ширину кнопки на всю ширину
+        child: ElevatedButton(
+          onPressed: () {
+            if (_formKey.currentState!.validate()) {
+              _login();
+            }
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.black, // Черный фон
+            side: const BorderSide(color: Colors.white, width: 1.0), // Белый контур
+            padding: const EdgeInsets.symmetric(vertical: 15.0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0), // Круглые углы
             ),
           ),
+          child: const Text('Login', style: TextStyle(fontSize: 18)),
         ),
+      ),
+      const SizedBox(height: 10),
+    ],
+    ),
+    ),
+    ],
+    ),
+    ),
+    ),
       ),
     );
   }
